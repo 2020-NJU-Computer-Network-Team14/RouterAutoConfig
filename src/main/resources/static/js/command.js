4a75840d0507e427d6f847850d922b1e9b1e7cbf
wzs82868996 <82868996@qq.com>
2020-12-29 19:16:46 +0800

    修改编辑ACL页面、JS

--- src/main/resources/static/js/command.js
+++ src/main/resources/static/js/command.js
@@ -1,16 +1,17 @@
 $("#command").keypress(function(e) {
 　　if (e.keyCode == 13) {
       $("#output").append($("#command").val() + "<br>")
-      // $.ajax({
-      //     type: "post",
-      //     url: "/command",
-      //     data: {
-      //         command: $("#command").val(),
-      //         router: $("title").html()
-      //     },
-      //     success: function (e) {
-      //     }
-      // });
+      $.ajax({
+          type: "post",
+          url: "/command",
+          data: {
+              command: $("#command").val(),
+              router: $("title").html()
+          },
+          success: function (e) {
+              $("#output").append(e)
+          }
+      });
       $("#command").val("")
   }
 })
--- src/main/resources/static/js/edit.js
+++ src/main/resources/static/js/edit.js
@@ -1,17 +1,15 @@
 let progress = $.AMUI.progress;
 
-$("#saveNum").click(function () {
+$("#saveStandard").click(function () {
     progress.start();
     $.ajax({
         type: "post",
-        url: "/",
+        url: "/standard",
         data: {
-            num: $("#num").val(),
-            permit: $("input[name='radioNum']:checked").val(),
-            ip: $("#ipNum").val(),
-            mask: $("#maskNum").val(),
-            protocol: $("#protocolNum").val(),
-            port: $("#portNum").val()
+            id: $("#idStandard").val(),
+            permit: $("input[name='radioStandard']:checked").val(),
+            ip: $("#ipStandard").val(),
+            mask: $("#maskStandard").val(),
         },
         success: function (e) {
             progress.done();
@@ -20,18 +18,20 @@ $("#saveNum").click(function () {
     });
 });
 
-$("#saveName").click(function () {
+$("#saveExtended").click(function () {
     progress.start();
     $.ajax({
         type: "post",
-        url: "/",
+        url: "/extend",
         data: {
-            name: $("#name").val(),
-            permit: $("input[name='radioNum']:checked").val(),
-            ip: $("#ipName").val(),
-            mask: $("#maskName").val(),
-            protocol: $("#protocolName").val(),
-            port: $("#portName").val()
+            id: $("#idExtended").val(),
+            permit: $("input[name='radioExtended']:checked").val(),
+            srcIp: $("#srcIpExtended").val(),
+            srcMask: $("#srcMaskExtended").val(),
+            destIp: $("#destIpExtended").val(),
+            destMask: $("#destMaskExtended").val(),
+            protocol: $("#protocol").val(),
+            port: $("#port").val()
         },
         success: function (e) {
             progress.done();
@@ -39,3 +39,37 @@ $("#saveName").click(function () {
         }
     });
 });
+
+function deleteList(id) {
+    progress.start();
+    $.ajax({
+        type: "post",
+        url: "/deleteList",
+        data: {
+            id: id
+        },
+        success: function (e) {
+            progress.done();
+            window.location.reload();
+        }
+    });
+}
+
+function deleteTerm(id) {
+    progress.start();
+    let i = id.slice(3, id.length)
+    $.ajax({
+        type: "post",
+        url: "/deleteTerm",
+        data: {
+            id: i.slice(0, i.indexOf("_")),
+            permit: $("#permit" + i).html(),
+            ip: $("#ip" + i).html(),
+            mask: $("#mask" + i).html(),
+        },
+        success: function (e) {
+            progress.done();
+            window.location.reload();
+        }
+    });
+}
