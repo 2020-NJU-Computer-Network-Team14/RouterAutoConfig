df18e4cc3049b1affc3e9d16e343fd03d34908d4
nianyuez <982740445@qq.com>
2021-01-12 18:20:34 +0800

    最终终终稿

--- src/main/java/cn/edu/nju/software/computerNetwork/controller/ConfigController.java
+++ src/main/java/cn/edu/nju/software/computerNetwork/controller/ConfigController.java
@@ -1,5 +1,7 @@
 package cn.edu.nju.software.computerNetwork.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
@@ -8,175 +10,165 @@ import org.springframework.web.bind.annotation.RestController;
 
 import cn.edu.nju.software.computerNetwork.service.RouterService;
 
-import java.util.List;
-
-/**  
- * @ClassName: ConfigController  
+/**
+ * @ClassName: ConfigController
  *
- * @Description: TODO(这里用一句话描述这个类的作用)  
+ * @Description: TODO(这里用一句话描述这个类的作用)
  *
- * @author 余定邦  
+ * @author 余定邦
  *
- * @date 2020年12月24日  
- *  
+ * @date 2020年12月24日
+ * 
  */
 
 @RestController
 @RequestMapping()
 public class ConfigController {
 
-    @Resource(name = "route-service")
-    private RouterService routerService;
+	@Resource(name = "route-service")
+	private RouterService routerService;
 
-    @RequestMapping(value = "telnet")
-    public String telnet(HttpServletRequest user) {
-        String routerName = user.getParameter("router");
-        String ip = user.getParameter("ip");
-        String telnetPassword = user.getParameter("password");
-        String enablePassword = user.getParameter("enable");
+	@RequestMapping(value = "telnet")
+	public String telnet(HttpServletRequest user) {
+		String routerName = user.getParameter("router");
+		String ip = user.getParameter("ip");
+		String telnetPassword = user.getParameter("password");
+		String enablePassword = user.getParameter("enable");
 
-        return routerService.connect(routerName, ip, 23, telnetPassword, enablePassword);
-    }
+		return routerService.connect(routerName, ip, 23, telnetPassword, enablePassword);
+	}
 
-    @RequestMapping(value = "interface")
-    public String configInterface(HttpServletRequest user) {
+	@RequestMapping(value = "interface")
+	public String configInterface(HttpServletRequest user) {
 
-        String routerName = user.getParameter("router");
-        String interfaceName = user.getParameter("interface");
-        String ip = user.getParameter("ip");
-        String mask = user.getParameter("mask");
+		String routerName = user.getParameter("router");
+		String interfaceName = user.getParameter("interface");
+		String ip = user.getParameter("ip");
+		String mask = user.getParameter("mask");
 
-        return routerService.configInterface(routerName, interfaceName, ip, mask);
-    }
+		return routerService.configInterface(routerName, interfaceName, ip, mask);
+	}
 
-    @RequestMapping(value = "route")
-    public String route(HttpServletRequest user) {
+	@RequestMapping(value = "route")
+	public String route(HttpServletRequest user) {
 
-        String routerName = user.getParameter("router");
-        String ipPrefix = user.getParameter("prefix");
-        String mask = user.getParameter("mask");
-        String nextHopIp = user.getParameter("next");
+		String routerName = user.getParameter("router");
+		String ipPrefix = user.getParameter("prefix");
+		String mask = user.getParameter("mask");
+		String nextHopIp = user.getParameter("next");
 
-        return routerService.configStaticRouting(routerName, ipPrefix, mask, nextHopIp);
-    }
+		return routerService.configStaticRouting(routerName, ipPrefix, mask, nextHopIp);
+	}
 
-    @RequestMapping(value = "binding")
-    public String binding(HttpServletRequest user) {
+	@RequestMapping(value = "binding")
+	public String binding(HttpServletRequest user) {
 
-        String routerName = user.getParameter("router");
-        String interfaceName = user.getParameter("interface");
-        String inOrOut = user.getParameter("in");
-        String aclName = user.getParameter("name");
+		String routerName = user.getParameter("router");
+		String interfaceName = user.getParameter("interface");
+		String inOrOut = user.getParameter("in");
+		String aclName = user.getParameter("name");
 
-        return routerService.configApplyAccessList(routerName, interfaceName, aclName, inOrOut);
-    }
+		return routerService.configApplyAccessList(routerName, interfaceName, aclName, inOrOut);
+	}
 
-    @RequestMapping(value = "noBinding")
-    public String noBinding(HttpServletRequest user) {
+	@RequestMapping(value = "noBinding")
+	public String noBinding(HttpServletRequest user) {
 
-        String routerName = user.getParameter("router");
-        String interfaceName = user.getParameter("interface");
-        String inOrOut = user.getParameter("in");
-        String aclName = user.getParameter("name");
+		String routerName = user.getParameter("router");
+		String interfaceName = user.getParameter("interface");
+		String inOrOut = user.getParameter("in");
+		String aclName = user.getParameter("name");
 
-        return routerService.configCancelAccessList(routerName, interfaceName, aclName, inOrOut);
-    }
+		return routerService.configCancelAccessList(routerName, interfaceName, aclName, inOrOut);
+	}
 
-    @RequestMapping(value = "ping")
-    public String ping(HttpServletRequest user) {
+	@RequestMapping(value = "ping")
+	public String ping(HttpServletRequest user) {
 
-        String routerName = user.getParameter("router");
-        String targetIp = user.getParameter("ip");
+		String routerName = user.getParameter("router");
+		String targetIp = user.getParameter("ip");
 
-        return routerService.ping(routerName, targetIp);
-    }
+		return routerService.ping(routerName, targetIp);
+	}
 
-     @RequestMapping(value = "command")
-     public String command(HttpServletRequest user) {
+	@RequestMapping(value = "command")
+	public String command(HttpServletRequest user) {
 
-         String router = user.getParameter("router");
-         String command = user.getParameter("command");
+		String router = user.getParameter("router");
+		String command = user.getParameter("command");
 
-         return routerService.execute(router, command);
-     }
+		return routerService.execute(router, command);
+	}
 
-    @RequestMapping(value = "show_acl")
-    public List<List<String>> showAccessList(HttpServletRequest user) {
+	@RequestMapping(value = "show_acl")
+	public List<List<String>> showAccessList(HttpServletRequest user) {
 
-        String routerName = user.getParameter("router");
+		String routerName = user.getParameter("router");
 
-        return routerService.showIpAccessList(routerName);
-    }
+		return routerService.showIpAccessList(routerName);
+	}
 
-    @RequestMapping(value = "show_interface")
-    public List<String> showInterface(HttpServletRequest user) {
+	@RequestMapping(value = "show_interface")
+	public List<String> showInterface(HttpServletRequest user) {
 
-        String routerName = user.getParameter("router");
-        String interfaceName = user.getParameter("interface");
+		String routerName = user.getParameter("router");
+		String interfaceName = user.getParameter("interface");
 
-        return routerService.showIpInterface(routerName,interfaceName);
-    }
+		return routerService.showIpInterface(routerName, interfaceName);
+	}
 
-    @RequestMapping(value = "standard")
-    public String standard(HttpServletRequest user) {
+	@RequestMapping(value = "standard")
+	public String standard(HttpServletRequest user) {
 
-        String routerName = user.getParameter("router");
-        String id = user.getParameter("id");
-        String permit = user.getParameter("permit");
-        String ip = user.getParameter("ip");
-        String mask = user.getParameter("mask");
+		String routerName = user.getParameter("router");
+		String id = user.getParameter("id");
+		String permit = user.getParameter("permit");
+		String ip = user.getParameter("ip");
+		String mask = user.getParameter("mask");
 
-        return routerService.configCreateAccessListStandard(routerName, id, permit, ip, mask);
-    }
+		return routerService.configCreateAccessListStandard(routerName, id, permit, ip, mask);
+	}
 
-    @RequestMapping(value = "extended")
-    public String extend(HttpServletRequest user) {
+	@RequestMapping(value = "extended")
+	public String extend(HttpServletRequest user) {
 
-        String routerName = user.getParameter("router");
-        String id = user.getParameter("id");
-        String permit = user.getParameter("permit");
-        String protocol = user.getParameter("protocol");
+		String routerName = user.getParameter("router");
+		String id = user.getParameter("id");
+		String permit = user.getParameter("permit");
+		String protocol = user.getParameter("protocol");
 
-        String srcIp = user.getParameter("srcIp");
-        String srcMask = user.getParameter("srcMask");
+		String srcIp = user.getParameter("srcIp");
+		String srcMask = user.getParameter("srcMask");
 
-        String destIp = user.getParameter("destIp");
-        String destMask = user.getParameter("destMask");
+		String destIp = user.getParameter("destIp");
+		String destMask = user.getParameter("destMask");
 
-        String relation = user.getParameter("relation");
-        String port = user.getParameter("port");
+		String relation = user.getParameter("relation");
+		String port = user.getParameter("port");
 
-        return routerService.configCreateAccessListExtend(routerName, id, permit, protocol, srcIp, srcMask, destIp,
-            destMask, relation, port);
-    }
+		return routerService.configCreateAccessListExtend(routerName, id, permit, protocol, srcIp, srcMask, destIp,
+				destMask, relation, port);
+	}
 
-    @RequestMapping(value = "deleteList")
-    public String deleteList(HttpServletRequest user) {
+	@RequestMapping(value = "deleteList")
+	public String deleteList(HttpServletRequest user) {
 
-        String routerName = user.getParameter("router");
-        String std = user.getParameter("std");
-        String id = user.getParameter("id");
+		String routerName = user.getParameter("router");
+		String std = user.getParameter("std");
+		String id = user.getParameter("id");
 
-        return routerService.configCancelAccessListGlobal(routerName, std, id);
-    }
+		return routerService.configCancelAccessListGlobal(routerName, std, id);
+	}
 
-    @RequestMapping(value = "deleteTerm")
-    public String deleteTerm(HttpServletRequest user) {
+	@RequestMapping(value = "deleteTerm")
+	public String deleteTerm(HttpServletRequest user) {
 
-        String routerName = user.getParameter("router");
-        String id = user.getParameter("id");
-        String std = user.getParameter("std");
-        String term = user.getParameter("term");
+		String routerName = user.getParameter("router");
+		String id = user.getParameter("id");
+		String std = user.getParameter("std");
+		String term = user.getParameter("term");
 
-        return routerService.configCancelAccessListGlobal(routerName, id, std, term);
-    }
+		return routerService.configCancelAccessListGlobal(routerName, id, std, term);
+	}
 
-//    @RequestMapping(value = "deleteExtendedTerm")
-//    public String deleteExtendedTerm(HttpServletRequest user) {
-//
-//        String routerName = user.getParameter("router");
-//        String id = user.getParameter("id");
-//        return routerService.configCancelAccessListGlobal(routerName, id, permit, protocol, srcIp, srcMask, destIp,
-//                destMask, "=", port);
-//    }
 }
--- src/main/java/cn/edu/nju/software/computerNetwork/telnet/TelnetService.java
+++ src/main/java/cn/edu/nju/software/computerNetwork/telnet/TelnetService.java
@@ -8,157 +8,126 @@ import java.util.List;
 
 import org.apache.commons.net.telnet.TelnetClient;
 
-/**  
- * @ClassName: TelnetService  
+/**
+ * @ClassName: TelnetService
  *
- * @Description: TODO(这里用一句话描述这个类的作用)  
+ * @Description: TODO(这里用一句话描述这个类的作用)
  *
- * @author 余定邦  
+ * @author 余定邦
  *
- * @date 2020年12月23日  
- *  
+ * @date 2020年12月23日
+ * 
  */
 public class TelnetService implements AutoCloseable {
 
-    private TelnetClient telnet;
-    private InputStream in;
-    private PrintStream out;
+	private TelnetClient telnet;
+	private InputStream in;
+	private PrintStream out;
 
-    public void connect(String ip, int port, String telnetPassword, String enablePassword) throws IOException {
-        telnet = new TelnetClient();
-        telnet.connect(ip, port);
-        in = telnet.getInputStream();
-        out = new PrintStream(telnet.getOutputStream());
+	public void connect(String ip, int port, String telnetPassword, String enablePassword) throws IOException {
+		telnet = new TelnetClient();
+		telnet.connect(ip, port);
+		in = telnet.getInputStream();
+		out = new PrintStream(telnet.getOutputStream());
 
-        login(telnetPassword, enablePassword);
-    }
+		login(telnetPassword, enablePassword);
+	}
 
-    /*
-     * 
-    User Access Verification
-    
-    Password: 
-     * 
-     * 
-    Router>enable
-    enable
-    % No password set
-    Router>
-    
-    User Access Verification
-    
-    Password: 123
-    
-    Router>enable
-    enable
-    Password: 1
-    
-    Password: CISCO
-    
-    Router#
-    
-     */
+	private void login(String telnetPassword, String enablePassword) throws IOException {
+		String str = getOutput(Arrays.asList(">"));
+		if (str.endsWith(":")) {
+			if (telnetPassword == null) {
+				throw new IllegalArgumentException("Telnet密码为空！");
+			}
+			inputPassword(telnetPassword, "Telnet");
+		}
+		String enableOutput = executeWithoutRemove("enable", Arrays.asList("Password:", ">"));
+		if (enableOutput.contains("% No password set")) {
+			throw new IllegalArgumentException("路由器未设置enable密码，请先设置路由器enable密码，再进行本操作！");
+		}
+		if (telnetPassword == null) {
+			throw new IllegalArgumentException("Enable密码为空！");
+		}
+		inputPassword(enablePassword, "Enable");
+	}
 
-    private void login(String telnetPassword, String enablePassword) throws IOException {
-        String str = getOutput(Arrays.asList(">"));
-        if (str.endsWith(":")) {
-            if (telnetPassword == null) {
-                throw new IllegalArgumentException("Telnet密码为空！");
-            }
-            inputPassword(telnetPassword, "Telnet");
-        }
-        String enableOutput = executeWithoutRemove("enable", Arrays.asList("Password:", ">"));
-        if (enableOutput.contains("% No password set")) {
-            throw new IllegalArgumentException("路由器未设置enable密码，请先设置路由器enable密码，再进行本操作！");
-        }
-        if (telnetPassword == null) {
-            throw new IllegalArgumentException("Enable密码为空！");
-        }
-        inputPassword(enablePassword, "Enable");
-    }
+	private void inputPassword(String telnetPassword, String name) throws IOException {
+		String passwordReturn = executeWithoutRemove(telnetPassword, Arrays.asList("Password:", "#"));
+		if (passwordReturn.endsWith(":")) {
+			throw new IllegalArgumentException(name + "密码不对！");
+		}
+	}
 
-    private void inputPassword(String telnetPassword, String name) throws IOException {
-        String passwordReturn = executeWithoutRemove(telnetPassword, Arrays.asList("Password:", "#"));
-        if (passwordReturn.endsWith(":")) {
-            throw new IllegalArgumentException(name + "密码不对！");
-        }
-    }
+	public String executeWithoutRemove(String command, List<String> prompts) throws IOException {
+		submitCommand(command);
+		return getOutput(prompts).toString();
+	}
 
-    public String executeWithoutRemove(String command, List<String> prompts) throws IOException {
-        submitCommand(command);
-        return getOutput(prompts).toString();
-    }
+	public String getOutput(List<String> prompts) throws IOException {
+		StringBuilder buf = new StringBuilder();
+		while (true) {
+			char ch = (char) in.read();
+			buf.append(ch);
+			if (bufIsEndWithPrompt(buf, prompts)) {
+				break;
+			}
+		}
+		return buf.toString();
+	}
 
-    public String getOutput(List<String> prompts) throws IOException {
-        StringBuilder buf = new StringBuilder();
-        while (true) {
-            char ch = (char)in.read();
-            buf.append(ch);
-            if (bufIsEndWithPrompt(buf, prompts)) {
-                break;
-            }
-        }
-        return buf.toString();
-    }
+	public String execute(String command, List<String> prompts) throws IOException {
+		return removeInputAndPrompt(executeWithoutRemove(command, prompts));
+	}
 
-    public String execute(String command, List<String> prompts) throws IOException {
-        return removeInputAndPrompt(executeWithoutRemove(command, prompts));
-    }
+	public String executeAndGetPrompt(String command, List<String> prompts) throws IOException {
+		return removeInput(executeWithoutRemove(command, prompts));
+	}
 
-    public String executeAndGetPrompt(String command, List<String> prompts) throws IOException {
-        return removeInput(executeWithoutRemove(command, prompts));
-    }
+	private String removeInputAndPrompt(String buf) {
+		int low = firstLine(buf), high = lastLine(buf);
+		return buf.substring(low + 1, high);
+	}
 
-    private String removeInputAndPrompt(String buf) {
-        int low = firstLine(buf), high = lastLine(buf);
-        return buf.substring(low + 1, high);
-    }
+	private String removeInput(String buf) {
+		int low = firstLine(buf);
+		return buf.substring(low);
+	}
 
-    private String removeInput(String buf) {
-        int low = firstLine(buf);
-        return buf.substring(low);
-    }
+	private int lastLine(String buf) {
+		for (int i = buf.length() - 1; i >= 0; --i) {
+			if (buf.charAt(i) == '\n') {
+				return i;
+			}
+		}
+		return 0;
+	}
 
-    private int lastLine(String buf) {
-        for (int i = buf.length() - 1; i >= 0; --i) {
-            if (buf.charAt(i) == '\n') {
-                return i;
-            }
-        }
-        return 0;
-    }
+	private int firstLine(String buf) {
+		for (int i = 0; i < buf.length(); ++i) {
+			if (buf.charAt(i) == '\n') {
+				return i;
+			}
+		}
+		return 0;
+	}
 
-    private int firstLine(String buf) {
-        for (int i = 0; i < buf.length(); ++i) {
-            if (buf.charAt(i) == '\n') {
-                return i;
-            }
-        }
-        return 0;
-    }
+	private boolean bufIsEndWithPrompt(StringBuilder buf, List<String> prompts) {
+		for (String prompt : prompts) {
+			if (buf.indexOf(prompt) >= 0) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    private boolean bufIsEndWithPrompt(StringBuilder buf, List<String> prompts) {
-        for (String prompt : prompts) {
-            if (buf.indexOf(prompt) >= 0) {
-                return true;
-            }
-        }
-        return false;
-    }
+	public void submitCommand(String command) {
+		out.println(command);
+		out.flush();
+	}
 
-    public void submitCommand(String command) {
-        out.println(command);
-        out.flush();
-    }
+	@Override
+	public void close() throws Exception {
+		telnet.disconnect();
+	}
 
-    @Override
-    public void close() throws Exception {
-        telnet.disconnect();
-    }
-
-    public static void main(String[] args) throws Exception {
-        try (TelnetService telnet = new TelnetService()) {
-            telnet.connect("192.168.10.3", 23, "123", "CISCO");
-        }
-    }
 }
